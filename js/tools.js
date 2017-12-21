9c32e2341d6d2cd0f8951b123bb98c6c0cc4296e
htmltest <struev.vs@gmail.com>
2017-12-21 09:08:12 +0300

    fix/update

--- js/tools.js
+++ js/tools.js
@@ -64,6 +64,7 @@ $(document).ready(function() {
         infinite: false,
         slidesToShow: 3,
         slidesToScroll: 3,
+        initialSlide: 3,
         prevArrow: '<button type="button" class="slick-prev"></button>',
         nextArrow: '<button type="button" class="slick-next"></button>',
         dots: false
