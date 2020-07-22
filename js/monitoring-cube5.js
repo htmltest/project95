31a1099092f3a5fdade19975ff613c7b9cd23f51
htmltest <struev.vs@gmail.com>
2020-07-22 14:38:29 +0300

    cube 14 face 1

--- js/monitoring-cube5.js
+++ js/monitoring-cube5.js
@@ -157,6 +157,7 @@ $(window).on('load resize', function() {
     face16_3_Redraw();
     face22_2_Redraw();
     face24_1_Redraw();
+    face14_1_Redraw();
 
 });
 
@@ -867,4 +868,201 @@ function face24_1_Redraw() {
         $('.face-39-ratio-window').remove();
     });
 
+}
+
+function face14_1_Redraw() {
+    var face1Labels = [];
+    var face1DataActually = [];
+    var face1DataForecast = [];
+
+    $('.face-14-1-container .face-1-chart-graph').html('');
+    $('.face-14-1-container .face-1-chart-labels').html('');
+
+    var itemWidth = 110;
+    if ($(window).width() < 1140) {
+        itemWidth = 80;
+    }
+
+    var itemMargin = 55;
+    if ($(window).width() < 1140) {
+        itemMargin = 40;
+    }
+
+    for (var i = 0; i < faceData14_1.length; i++) {
+        var curData = faceData14_1[i];
+        face1Labels.push(curData.year);
+
+        if (curData.type == 'actually') {
+            if (curData.count !== undefined) {
+                face1DataActually.push(Number(curData.count));
+            } else {
+                face1DataActually.push(null);
+            }
+            if (curData.countf !== undefined) {
+                face1DataForecast.push(Number(curData.countf));
+            } else {
+                face1DataForecast.push(null);
+            }
+        } else {
+            face1DataActually.push(null);
+            face1DataForecast.push(Number(curData.countf));
+        }
+    }
+
+    $('.face-14-1-container .face-1-chart').width(face1Labels.length * itemWidth + itemMargin);
+
+    var minPlace = 9999;
+    var maxPlace = 0;
+    var curScroll = 0;
+
+    for (var i = 0; i < face1DataActually.length; i++) {
+        if (face1DataActually[i] != null) {
+            if (face1DataActually[i] < minPlace) {
+                minPlace = face1DataActually[i];
+            }
+            if (face1DataActually[i] > maxPlace) {
+                maxPlace = face1DataActually[i];
+            }
+        }
+    }
+
+    for (var i = 0; i < face1DataForecast.length; i++) {
+        if (face1DataForecast[i] != null) {
+            if (face1DataForecast[i] < minPlace) {
+                minPlace = face1DataForecast[i];
+            }
+            if (face1DataForecast[i] > maxPlace) {
+                maxPlace = face1DataForecast[i];
+            }
+        }
+    }
+
+    function angle_point(a, b, c) {
+        var x1 = a[0] - b[0];
+        var x2 = c[0] - b[0];
+        var y1 = a[1] - b[1];
+        var y2 = c[1] - b[1];
+
+        var d1 = Math.sqrt(x1 * x1 + y1 * y1);
+        var d2 = Math.sqrt(x2 * x2 + y2 * y2);
+        return Math.acos((x1 * x2 + y1 * y2) / (d1 * d2)) * 180 / Math.PI;
+    }
+
+    var curLastActually = -1;
+    for (var i = 0; i < face1DataActually.length; i++) {
+        if (face1DataActually[i] != null) {
+            curLastActually++;
+            var curX = (i * itemWidth) + itemMargin;
+            curScroll = curX;
+            var curY = ((face1DataActually[i] - maxPlace) / (minPlace - maxPlace)) * $('.face-14-1-container .face-1-chart-graph').height();
+            if (face1DataActually[i - 1] != null) {
+                var prevX = ((i - 1) * itemWidth) + itemMargin;
+                var prevY = ((face1DataActually[i - 1] - maxPlace) / (minPlace - maxPlace)) * $('.face-14-1-container .face-1-chart-graph').height();
+                var curWidth = Math.sqrt(Math.pow((curX - prevX), 2) + Math.pow((curY - prevY), 2));
+                var curAngle = angle_point([curX, curY], [prevX, prevY], [curX, prevY]);
+                if (curY < prevY) {
+                    curAngle = -curAngle;
+                }
+                $('.face-14-1-container .face-1-chart-graph').append('<div class="face-1-chart-line active" style="left:' + prevX + 'px; top:' + prevY + 'px; width:' + curWidth + 'px; transform:rotate(' + curAngle + 'deg)"></div>');
+            }
+            $('.face-14-1-container .face-1-chart-graph').append('<div class="face-1-chart-point active" style="left:' + curX + 'px; top:' + curY + 'px"><span><strong>' + face1DataActually[i] + '<em>&nbsp;ед.</em></strong></span></div>');
+        }
+    }
+
+    var curLastForecast = -1;
+    for (var i = 0; i < face1DataForecast.length; i++) {
+        if (face1DataForecast[i] != null) {
+            if (curLastForecast < 0) {
+                curLastForecast = i;
+            }
+            var curX = (i * itemWidth) + itemMargin;
+            var curY = ((face1DataForecast[i] - maxPlace) / (minPlace - maxPlace)) * $('.face-14-1-container .face-1-chart-graph').height();
+            if (face1DataForecast[i - 1] != null) {
+                var prevX = ((i - 1) * itemWidth) + itemMargin;
+                var prevY = ((face1DataForecast[i - 1] - maxPlace) / (minPlace - maxPlace)) * $('.face-14-1-container .face-1-chart-graph').height();
+                var curWidth = Math.sqrt(Math.pow((curX - prevX), 2) + Math.pow((curY - prevY), 2));
+                var curAngle = angle_point([curX, curY], [prevX, prevY], [curX, prevY]);
+                if (curY < prevY) {
+                    curAngle = -curAngle;
+                }
+                $('.face-14-1-container .face-1-chart-graph').append('<div class="face-1-chart-line" style="left:' + prevX + 'px; top:' + prevY + 'px; width:' + curWidth + 'px; transform:rotate(' + curAngle + 'deg)"></div>');
+            }
+            $('.face-14-1-container .face-1-chart-graph').append('<div class="face-1-chart-point" style="left:' + curX + 'px; top:' + curY + 'px"><span><strong>' + face1DataForecast[i] + '<em>&nbsp;ед.</em></strong></span></div>');
+        }
+    }
+
+    for (var i = 0; i < face1Labels.length; i++) {
+        if (face1DataActually[i] != null) {
+            $('.face-14-1-container .face-1-chart-labels').append('<div class="face-1-chart-year" style="left:' + (i * itemWidth + itemMargin) + 'px"><strong>' + face1Labels[i] + '</strong></div>');
+        } else {
+            $('.face-14-1-container .face-1-chart-labels').append('<div class="face-1-chart-year" style="left:' + (i * itemWidth + itemMargin) + 'px"><span>' + face1Labels[i] + '</span></div>');
+        }
+    }
+
+    $('.face-14-1-container').mCustomScrollbar('destroy');
+    $('.face-14-1-container').mCustomScrollbar({
+        axis: 'x',
+        setLeft: '-' + (curScroll - $('.face-14-1-container').width() / 2) + 'px',
+        scrollButtons: {
+            enable: true
+        }
+    });
+
+    face14_1_RedrawFooter();
+}
+
+$(document).ready(function() {
+    $('.face-14-1-year-current').click(function(e) {
+        $(this).parent().toggleClass('open');
+    });
+
+    $(document).click(function(e) {
+        if ($(e.target).parents().filter('.face-14-1-year').length == 0) {
+            $('.face-14-1-year').removeClass('open');
+        }
+    });
+
+    $('.face-14-1-year ul li a').click(function(e) {
+        var curLi = $(this).parent();
+        if (!curLi.hasClass('active')) {
+            $('.face-14-1-year ul li.active').removeClass('active');
+            curLi.addClass('active');
+            $('.face-14-1-year-current').html($(this).html());
+            face14_1_RedrawFooter();
+        }
+        $('.face-14-1-year').removeClass('open');
+        e.preventDefault();
+    });
+});
+
+function face14_1_RedrawFooter() {
+    var curYear = $('.face-14-1-year-current').html();
+    var curData = null;
+    for (var i = 0; i < faceData14_1.length; i++) {
+        if (faceData14_1[i].year == curYear) {
+            curData = faceData14_1[i].data;
+        }
+    }
+
+    if (curData !== null) {
+        var newHTML = '';
+
+        for (var i = 0; i < curData.length; i++) {
+            var curRow = curData[i];
+            newHTML +=  '<div class="face-14-1-list-row">';
+            newHTML +=      '<div class="face-14-1-list-icon"><img src="' + curRow.icon + '" alt="" /></div>';
+            newHTML +=      '<div class="face-14-1-list-eng">' + curRow.eng + '</div>';
+            newHTML +=      '<div class="face-14-1-list-rus">' + curRow.rus + '</div>';
+            newHTML +=      '<div class="face-14-1-list-places">';
+            for (var j = 0; j < curRow.list.length; j++) {
+                newHTML +=      '<div class="face-14-1-list-country"><img src="' + curRow.list[j].flag + '" alt="" /><span>' + curRow.list[j].name + '</span></div>';
+            }
+            newHTML +=      '</div>';
+            newHTML +=  '</div>';
+        }
+
+        $('.face-14-1-list').html(newHTML);
+
+        $('.cube').css({'margin-bottom': $('.cube-face.active').find('.cube-face-footer').outerHeight()});
+    }
 }
